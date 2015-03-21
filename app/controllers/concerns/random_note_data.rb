3c7d489851c84da1867f9b9df692da98cb643ad5
Clete Blackwell II <clete2@gmail.com>
2015-03-21 10:26:58 -0500

    Notes sample data is now random

---
+++ app/controllers/concerns/random_note_data.rb
@@ -0,0 +1,56 @@
+class RandomNoteData
+  @@note_amounts = [25, 50]
+  @@loan_statuses_weighted = ['In Review', 'In Funding', 'Issuing', 'Issued',
+                              'Issued', 'Current', 'Current', 'Current', 'Current',
+                              'Current', 'Current', 'Current', 'Current',
+                              'Current', 'Current', 'In Grace Period', 'Late (16-30)',
+                              'Late (31-120)', 'Fully Paid', 'Fully Paid',
+                              'Fully Paid', 'Default', 'Charged Off']
+  @@loan_lengths = [36, 60]
+  @@grades = %w(A1 A2 A3 A4 A5 B1 B2 B3 B4 B5 C1 C2 C3 C4
+             C5 D1 D2 D3 D4 D5 E1 E2 E3 E4 E5 F1 F2 F3 F4 F5 G1 G2 G3 G4 G5)
+  @@credit_trends = %w(UP FLAT DOWN)
+
+  def generate_random_notes(count)
+    notes_list = []
+
+    (0..count).each {
+      note_amount = @@note_amounts.sample
+      loan_length = @@loan_lengths.sample
+      loan_status = @@loan_statuses_weighted.sample
+      if ['Charged Off', 'Default'].include? loan_status
+        payments_received = '%.2f' % rand(1.00..(note_amount * 0.8))
+      else
+        payments_received = '%.2f' % rand(1.00..(note_amount * 1.2))
+      end
+
+
+      note = {
+          'loanId' => rand(10000000..99999999),
+          'noteId' => rand(10000000..99999999),
+          'orderId' => rand(10000000..99999999),
+          'interestRate' => ('%.1f' % rand(5.0..22.0)).to_f,
+          'loanLength' => loan_length,
+          'loanStatus' => loan_status,
+          'grade' => @@grades.sample,
+          'loanAmount' => rand(1000..35000),
+          'noteAmount' => note_amount,
+          'paymentsReceived' => payments_received.to_f,
+          'accruedInterest' => ('%.2f' % rand(0.00..0.50)).to_f,
+          'issueDate' => rand(loan_length.months.ago..Time.now).strftime('%m/%d/%Y'),
+          'orderDate' => rand(loan_length.months.ago..Time.now).strftime('%m/%d/%Y'),
+          'nextPaymentDate' => rand(Time.now..1.month.from_now).strftime('%m/%d/%Y'),
+          'creditTrend' => @@credit_trends.sample
+      }
+
+      notes_list.push(note)
+    }
+
+    # TODO: Maybe move this out of the loan
+    notes_list[0]['mockedData'] = true
+
+    data = {
+        'myNotes' => notes_list
+    }
+  end
+end
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,6 @@
 class NotesController < ApplicationController
+  require 'concerns/random_note_data'
+
   def index
   end
 
@@ -11,48 +13,8 @@ class NotesController < ApplicationController
           {:headers => {'Authorization' => cookies[:api_key]}}
       )
     else
-      # Mock some data
-      # TODO: Generate random data for mocked data!
-      data = {
-          'myNotes' =>
-              [
-                  {
-                      'mockedData' => true,
-                      'loanId' => 12353251,
-                      'noteId' => 7452342,
-                      'orderId' => 31284517,
-                      'interestRate' => 12.5,
-                      'loanLength' => 36,
-                      'loanStatus' => 'Charged Off',
-                      'grade' => 'B2',
-                      'loanAmount' => 13000,
-                      'noteAmount' => 25,
-                      'paymentsReceived' => 22.52,
-                      'accruedInterest' => 0.12,
-                      'issueDate' => '01/01/2013',
-                      'orderDate' => '12/27/2012',
-                      'nextPaymentDate' => '03/24/2015',
-                      'creditTrend' => 'UP'
-                  },
-                  {
-                      'loanId' => 234234,
-                      'noteId' => 23423234,
-                      'orderId' => 5236526,
-                      'interestRate' => 16.2,
-                      'loanLength' => 60,
-                      'loanStatus' => 'Current',
-                      'grade' => 'D1',
-                      'loanAmount' => 22000,
-                      'noteAmount' => 50,
-                      'accruedInterest' => 0.22,
-                      'paymentsReceived' => 12.01,
-                      'issueDate' => '03/10/2015',
-                      'orderDate' => '03/15/2015',
-                      'nextPaymentDate' => '04/26/2015',
-                      'creditTrend' => 'UP'
-                  }
-              ]
-      }
+      random_note_data = RandomNoteData.new
+      data = random_note_data.generate_random_notes(100)
     end
     render json: data
   end
